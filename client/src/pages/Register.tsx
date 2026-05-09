44f4dcb106a8edd20e273fd15b87eecc3b8ae451
HdrLucie <hlucie@student.42.fr>
2026-05-09 20:46:08 +0200

    fix: login fetch by adding headers and content-type

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -10,6 +10,9 @@ function NameForm() {
 			console.log("TRY");
 			const response = await fetch("http://127.0.0.1:8080/login", {
 				method:"POST",
+				headers : {
+					"Content-Type": "application/json",
+				},
 				body: JSON.stringify({
 					name : login,
 				}), 
